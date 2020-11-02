28f8c3940fa57e58b17ef886810aa339ee59deb9
rednguyen <tvn004@ucsd.edu>
2020-11-02 05:59:55 -0600

    MyThirdCommit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import './App.css';
-
+import Tsunami from "./Figure.jpg"
 
 
 function App() {
     return (
       <div className="App">
         <div className="TopPage"><NavBar /></div>
+        <div className = "body">
+          <img  src={Tsunami} height="1000" width="1000"/>
+        </div>
         <div className = "Wrapper">
           <div className = "BottomPage"><Footer /></div>
         </div>
