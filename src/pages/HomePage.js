c899c5d43b971f0832f91135736fea74c35f193f
Red Nguyen <Red Nguyen>
2020-11-05 02:00:19 -0600

    fix css file

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,14 +8,12 @@ import { Component } from 'react';
 class HomePage extends Component {
     render(){
         return (
-            <div className="App">
+            <div className="Wrapper">
               <div className="TopPage"><NavBar /></div>
               <div className = "body">
                 <img  src={Tsunami} height="1000" width="1000"/>
               </div>
-              <div className = "Wrapper">
                 <div className = "BottomPage"><Footer /></div>
-              </div>
             </div>
           )
     };  
